2318a2d1a7bc27539bd8e354823047d71aa85c92
Peter-O-Neill <poneill@myhome.ie>
2014-09-18 11:05:14 +0100

    First commit

---
+++ KittenView.Core/Services/Kitten.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace KittenView.Core.Services
+{
+    public class Kitten
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
